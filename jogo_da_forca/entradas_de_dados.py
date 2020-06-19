31b597323a5de72694be9b2fd3c2fe724842d37f
gabriel301297 <61299520+gabriel301297@users.noreply.github.com>
2020-06-19 19:45:17 -0300

    Add files via upload

---
+++ jogo_da_forca/entradas_de_dados.py
@@ -0,0 +1,15 @@
+def q_jogadores_nomes():
+    jogadores = []
+    while true:
+        n_jogadores = input('Informe o número de jogadores: ')
+        if n_jogadores.isnumeric():
+            n_jogadores - int(n_jogadores)
+            break
+        else:
+            print('Informe um número!!!')
+
+    for i in range(0, n_jogadores):
+        jogador - input('Informe o nome do jogadore:')
+    return jogadores
+
+def while true
